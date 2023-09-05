938f1a2e1a669a1e971ad9ebf9b07b22a8a082f0
wujiajun <2656112018@qq.com>
2023-09-05 10:15:12 +0800

    🎉 init: 新建项目

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+	</view>
+	<view class="">
+		
+	</view>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+	
+</style>
