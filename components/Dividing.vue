b291eff3263827203fe3b29d26923f7f6b19fd2a
wujiajun <2656112018@qq.com>
2023-09-08 16:44:05 +0800

    ✨ feat: 分割线组件

---
+++ components/Dividing.vue
@@ -0,0 +1,14 @@
+<!-- 分割线 -->
+<template>
+  <view class="dividing-line"></view>
+</template>
+<script setup>
+
+</script>
+<style lang='scss' scoped>
+.dividing-line {
+  // 虚线
+  width: 100%;
+  border-top: 1px dashed #DEDEDD;
+}
+</style>
